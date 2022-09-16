10da20a10bb189d4fc241e2fc01dc710e09d031f
Robson Souza <robson.gallina@gmail.com>
2022-09-15 23:34:16 -0300

    [RGS] aula 04 - mobile and web

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -6,9 +6,9 @@ import { THEME } from '../../theme';
 
 export interface GameCardProps {
     id: string;
-    name: string;
-    ads: string;
-    cover: ImageSourcePropType;
+    title: string;
+    _count: {ads: number};
+    bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps{
@@ -18,10 +18,10 @@ interface Props extends TouchableOpacityProps{
 export function GameCard({ data, ...rest }: Props) {
   return (
     <TouchableOpacity style={styles.container} {...rest}>
-        <ImageBackground style={styles.cover} source={data.cover}>
+        <ImageBackground style={styles.cover} source={{uri: data.bannerUrl}}>
             <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
-                <Text style={styles.name}>{data.name}</Text>
-                <Text style={styles.ads}>{data.ads} Anúncios</Text>
+                <Text style={styles.name}>{data.title}</Text>
+                <Text style={styles.ads}>{data._count.ads} Anúncios</Text>
             </LinearGradient>
         </ImageBackground>
     </TouchableOpacity>
